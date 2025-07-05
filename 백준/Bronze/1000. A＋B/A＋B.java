1bc2d7ba728cad4f13cebad05c2f05e3a00093fb
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-07-05 22:58:35 +0900

    [Bronze V] Title: A+B, Time: 180 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
-
-class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
