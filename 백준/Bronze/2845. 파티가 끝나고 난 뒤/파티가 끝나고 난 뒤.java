d832b116fb11b96178cae762869fc38e305b5d18
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-07-13 19:35:28 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 180 ms, Memory: 18156 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for (int i = 0; i < 5; i++) {
+            int x = sc.nextInt();
+            System.out.print(x - (n*m) + " ");
+        }
+    }
+}
