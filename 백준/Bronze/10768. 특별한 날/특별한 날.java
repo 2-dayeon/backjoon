a943edfb5130e0a06ea72b29b46bb099012c8004
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-07-14 09:02:21 +0900

    [Bronze IV] Title: 특별한 날, Time: 168 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a < 2 || a == 2 && b < 18) System.out.println("Before");
+        else if (a == 2 && b == 18) System.out.println("Special");
+        else System.out.println("After");
+    }
+}
