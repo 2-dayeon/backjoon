392356c6d631364d26b88e7b78d91753bb6cc8ab
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-07-18 23:55:51 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 472 ms, Memory: 24332 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = n-1; i > -1; i--) {
+            for (int j = 0; j < i; j++) System.out.print(" ");
+            for (int h = 0; h < n-i; h++) System.out.print("*");
+            for (int k = 0; k < n-(i+1); k++) System.out.print("*");
+            System.out.println();
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) System.out.print(" ");
+            for (int h = 0; h < n-i; h++) System.out.print("*");
+            for (int k = 0; k < n-(i+1); k++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
