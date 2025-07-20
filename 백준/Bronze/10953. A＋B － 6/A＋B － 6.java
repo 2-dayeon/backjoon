ab7d168e0e2218bf03bab550768bde6742d2ff49
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-07-20 21:21:28 +0900

    [Bronze III] Title: A+B - 6, Time: 196 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            String[] li = s.split(",");
+            int a = Integer.parseInt(li[0]);
+            int b = Integer.parseInt(li[1]);
+            System.out.println(a + b);
+        }
+    }
+}
