bded8cf88a5e457bed3a15a659f2b28af380f1d0
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-07-22 19:40:20 +0900

    [Bronze III] Title: TGN, Time: 200 ms, Memory: 18112 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            if (b-c > a) System.out.println("advertise");
+            else if (b-c == a) System.out.println("does not matter");
+            else System.out.println("do not advertise");
+        }
+    }
+}
