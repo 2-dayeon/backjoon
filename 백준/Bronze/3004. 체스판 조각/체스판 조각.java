50de40d707eac5ce97728e8b1d15395277c4bb01
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-08-04 23:28:49 +0900

    [Bronze III] Title: 체스판 조각, Time: 212 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/3004. 체스판 조각/체스판 조각.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = n/2;
+        int b = n-a;
+        System.out.println((a + 1) * (b + 1));
+    }
+}
