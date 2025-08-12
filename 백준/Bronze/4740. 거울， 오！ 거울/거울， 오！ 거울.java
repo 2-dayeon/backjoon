cb4030b63de8b8e9d74454061a9a2e5017e40a14
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-08-12 19:57:16 +0900

    [Bronze III] Title: 거울, 오! 거울, Time: 172 ms, Memory: 17408 KB -BaekjoonHub

---
+++ 백준/Bronze/4740. 거울， 오！ 거울/거울， 오！ 거울.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String s = sc.nextLine();
+            if (s.equals("***")) break;
+            for (int i = s.length()-1; i > -1; i--) {
+                System.out.print(s.charAt(i));
+            }
+            System.out.println();
+        }
+    }
+}
