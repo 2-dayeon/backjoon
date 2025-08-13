972751b967d087adf621658a18e1e55afd4f1393
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-08-13 23:38:24 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 172 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.printf("You get %d piece(s) and your dad gets %d piece(s).\n", a/b , a%b);
+        }
+    }
+}
