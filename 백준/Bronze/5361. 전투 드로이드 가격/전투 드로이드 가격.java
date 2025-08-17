8d6377df99c6eb9e47e67f0adc27eed9be3d0622
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-08-18 08:42:49 +0900

    [Bronze III] Title: 전투 드로이드 가격, Time: 176 ms, Memory: 17860 KB -BaekjoonHub

---
+++ 백준/Bronze/5361. 전투 드로이드 가격/전투 드로이드 가격.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            double sum = 0;
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+            int e = sc.nextInt();
+            sum += (a*350.34) + (b*230.90) + (c*190.55) + (d*125.30) + (e*180.90);
+            System.out.printf("$%.2f\n",sum);
+        }
+    }
+}
