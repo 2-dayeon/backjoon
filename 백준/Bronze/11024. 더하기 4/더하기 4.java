d0dedf422ff22558b009fba0bee097c77ba48c3c
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-08-19 09:39:15 +0900

    [Bronze III] Title: 더하기 4, Time: 244 ms, Memory: 18244 KB -BaekjoonHub

---
+++ 백준/Bronze/11024. 더하기 4/더하기 4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            String s = sc.nextLine();
+            String[] arr = s.split(" ");
+            for (String str : arr) {
+                sum += Integer.parseInt(str);
+            }
+            System.out.println(sum);
+        }
+    }
+}
