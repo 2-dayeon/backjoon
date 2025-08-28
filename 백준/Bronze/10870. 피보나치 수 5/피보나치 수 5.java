5184fbc965bdf3da968e06c56150fe49997e45e6
2-dayeon <all.kite091106@dgsw.hs.kr>
2025-08-28 14:20:27 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 188 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> li = new ArrayList<>();
+        int n = sc.nextInt();
+        int sum = 0;
+        li.add(0);
+        li.add(1);
+        for (int i = 2; i <= n; i++) {
+            li.add(li.get(i - 1) + li.get(i - 2));
+        }
+        System.out.println(li.get(n));
+    }
+}
